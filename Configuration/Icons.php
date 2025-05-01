71bbe924feccf796888314948ea3b9f070797604
Martin Keller <martin.keller@taketool.de>
2025-05-01 10:05:03 +0200

    optimize mod icon

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -7,6 +7,6 @@ use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 return [
     'tx_webalizer-backend-module' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:webalizer/Resources/Public/Icons/Extension.svg',
+        'source' => 'EXT:webalizer/Resources/Public/Icons/module-indexed_search.svg',
     ],
 ];
